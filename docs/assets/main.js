8d9cdc80c7f6b62c1618d107064f9938fd019754
zuripabon <zurisadai.pabon@gmail.com>
2023-01-05 20:55:45 +0100

    upload docs

--- docs/assets/main.js
+++ docs/assets/main.js
@@ -18,11 +18,8 @@ const q = (parentSelector, ...q) => document.querySelector(`${parentSelector}${q
 
 function setupUnity(selector, callback) {
     const gameCanvas = q(selector, 'canvas');
-    const logoElement = q(selector, '.logo');
-    const progressBarElement = q(selector, '.progress');
-    const progressBarFullElement = q(selector, '.progress .full');
-    const progressBarEmptyElement = q(selector, '.progress .empty');
-    const splashScreenElement = q(selector, '.splash');
+    const logoElement = q(selector, '.loading .container .logo');
+    // const audioElement = q(selector, '.loading .container .clip');
 
     const buildUrl = "Build";
     const loaderUrl = buildUrl + "/docs.loader.js";
@@ -40,25 +37,12 @@ function setupUnity(selector, callback) {
     const script = document.createElement("script");
     script.src = loaderUrl;
     script.onload = () => {
-        createUnityInstance(gameCanvas, config, (progress) => {
-            progressBarFullElement.style.width = (100 * progress) + "%";
-            progressBarEmptyElement.style.width = (100 * (1 - progress)) + "%";
-        }).then((unityInstance) => {
+        createUnityInstance(gameCanvas, config).then((unityInstance) => {
             info("Game loaded");
-            
-            splashScreenElement.style.display =  "block";
-         
-            setTimeout(()=>{
-                progressBarFullElement.style.width = 93
-            },1*1000);
-            setTimeout(()=>{
-                progressBarFullElement.style.width = 96
-            },2*1000);
             setTimeout(() => {
-                progressBarFullElement.style.width = 100
-                progressBarElement.style.display = "none";
-                logoElement.style.display =  "none";
-                splashScreenElement.style.display =  "none";
+                // audioElement.remove();
+                logoElement.style.display = "none";
+                toggleCanvasView();
             }, 3*1000);
             callback(unityInstance);
         }).catch((message) => {
@@ -71,23 +55,30 @@ function setupUnity(selector, callback) {
 }
 
 function resizeCanvas(selector, unityInstance){
-
-    const gameContainer = q(selector);
-
-    const initialDimensions = {
-        width: parseInt(gameContainer.style.width, 10), 
-        height: parseInt(gameContainer.style.height, 10)
-    };
-
-    let gCanvasElement = null;
-
-    if(window.screen){
-        screen.orientation.lock('landscape');
-    }
-
+    window.onresize = function(){  toggleCanvasView(); }
 };
 
-(function start(selector="#gameContainer"){
+function toggleCanvasView(){
+    const loadingElement = q('.main .loading');
+    const toggleOrientationElement = q('.main .loading .container .landscape-loading');
+
+    if(isLandscapeOrientation()){
+        loadingElement.style.display =  "none";
+        toggleOrientationElement.style.display = " none";
+        loadingElement.style.backgroundColor =  "#black";
+    }
+    else{
+        loadingElement.style.display =  "block";
+        toggleOrientationElement.style.display = " block";
+        loadingElement.style.backgroundColor =  "rgba(199, 57, 57, 0.86)";
+    }
+}
+
+function isLandscapeOrientation(){
+    return window.innerWidth > window.innerHeight;
+}
+
+(function start(selector=".main"){
     info("Hi ;)", {fontSize: 14});
 
     setupUnity(selector, (unityInstance)=> resizeCanvas(selector, unityInstance));
